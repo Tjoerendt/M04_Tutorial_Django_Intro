c48c9eb10a0fca16ad9ddfb1e7f327830a4d9cfc
Tyler Joerendt <tjoerendt@ivytech.edu>
2024-07-21 21:34:58 -0400

    updated urls, views, and forms

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('', include('blog.urls')),
 ]
 
